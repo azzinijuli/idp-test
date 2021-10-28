13b606138e4f19a3a1aaa80594f94e9cd4ca9113
Juli Azzini <jazzini@teco.com.ar>
2021-10-28 16:07:44 -0300

    jwt front

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,7 +2,7 @@ export default function Dashboard({ jwt }) {
   return (
     <>
       <div>Redirection success</div>
-      <div>JWT parsed: ${}</div>
+      <div>JWT: ${jwt}</div>
     </>
   );
 }
