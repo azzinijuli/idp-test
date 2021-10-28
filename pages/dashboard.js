e505cbfd17425d8dc6523bccf5fdc009b346a3ca
Juli Azzini <jazzini@teco.com.ar>
2021-10-28 16:13:15 -0300

    jwt library

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,10 @@
+import jwt_decode from "jwt-decode";
+
 export default function Dashboard({ jwt }) {
+  const token = jwt;
+  const decoded = jwt_decode(token);
+  console.log(decoded);
+
   return (
     <>
       <div>Redirection success</div>
