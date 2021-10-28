e5fe044f42688a96a61b71313b97f7cb32507a18
Juli Azzini <jazzini@teco.com.ar>
2021-10-28 14:49:28 -0300

    parse jwt

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,24 @@
 export default function Dashboard({ jwt }) {
-  console.log(jwt);
-  return <div>Redirection success</div>;
+  function parseJwt(jwt) {
+    const base64Url = jwt.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      Buffer.from(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    const result = JSON.parse(jsonPayload);
+    return result;
+  }
+  return (
+    <>
+      <div>Redirection success</div>
+      <div>JWT parsed: ${parseJwt()}</div>
+    </>
+  );
 }
 
 export async function getServerSideProps(context) {
