c2a7799d8ecefb455b5f803d15e23f6c1b8541ac
Juli Azzini <jazzini@teco.com.ar>
2021-10-29 16:10:47 -0300

    add nextauth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth/next";
+
+export default NextAuth({
+  providers: [
+    {
+      id: "idp",
+      name: "Portal Unificado",
+      type: "oauth",
+      wellKnown:
+        "https://idpsesion.telecom.com.ar/openam/oauth2/realms/convergente/.well-known/openid-configuration",
+      authorization: { params: { scope: "openid email profile" } },
+      idToken: true,
+      checks: ["pkce", "state"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    },
+  ],
+});
