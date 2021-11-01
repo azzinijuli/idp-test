454127a1cd44b86fc40de434dcb32a1a70b5870b
Juli Azzini <jazzini@teco.com.ar>
2021-11-01 12:08:04 -0300

    try sign in next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,19 +6,29 @@ export default NextAuth({
       id: "idp",
       name: "Portal Unificado",
       type: "oauth",
-      wellKnown:
-        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/.well-known/openid-configuration",
-      authorization: { params: { scope: "openid email profile" } },
+      version: "2.0",
+      scope: "openid email",
+      state: true,
+      protection: "state",
+      params: { grant_type: "authorization_code" },
       idToken: true,
-      checks: ["pkce", "state"],
-      profile(profile) {
+      authorizationUrl:
+        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/authorize",
+      accessTokenUrl:
+        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/access_token",
+      requestTokenUrl:
+        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/connect/jwk_uri",
+      profileUrl:
+        "https://idpsesiont.telecom.com.ar:443/openam/oauth2/convergente/userinfo",
+      profile(profile, tokens) {
         return {
-          id: profile.sub,
+          id: profile.id,
           name: profile.name,
           email: profile.email,
-          image: profile.picture,
         };
       },
+      clientId: process.env.NEXT_PUBLIC_IDP_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_IDP_CLIENT_SECRET,
     },
   ],
 });
